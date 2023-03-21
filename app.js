2e492d993bff7f5b4418b6058ce2a6718db20eaf
Harsh Kumar <89272576+harshkumaragarwal@users.noreply.github.com>
2023-03-22 00:58:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,8 +49,8 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    expires: Date.now() + 1000 * 60 * 60 * 24 ,
-    maxAge: 1000 * 60 * 60 * 24 
+    expires: Date.now() + 1000 * 60 * 60 * 24 *7,
+    maxAge: 1000 * 60 * 60 * 24 *7;
   },
 };
 
