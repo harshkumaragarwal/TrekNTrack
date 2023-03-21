7880081a3696c5bb6029afb976c36ddee3a023bb
Harsh Kumar <89272576+harshkumaragarwal@users.noreply.github.com>
2023-03-22 01:57:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
-const session = require("express-session");
+const session = require("cookie-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
@@ -50,7 +50,7 @@ const sessionConfig = {
   cookie: {
     httpOnly: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 *7,
-    maxAge: 1000 * 60 * 60 * 24 *7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
 
