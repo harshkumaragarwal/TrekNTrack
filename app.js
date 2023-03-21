a832d12ce1db95b96dde6c72c74a9918baac812e
harshkumaragarwal <89272576+harshkumaragarwal@users.noreply.github.com>
2023-03-22 00:52:09 +0530

    Master

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    expires: Date.now() + 1000 * 60 * 60 * 24 ,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
