3d95a771eb18b0325d913f6d3e6d596e023beb5d
harshkumaragarwal <89272576+harshkumaragarwal@users.noreply.github.com>
2023-09-21 03:21:16 +0530

    commit

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,12 +1,13 @@
 const Campground = require("../models/campground");
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-const axios = require('axios') 
+const axios = require("axios");
 const mapBoxToken = process.env.MAPBOX_TOKEN;
 const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 const { cloudinary } = require("../cloudinary");
 const Review = require("../models/review");
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({}).populate("popupText");
+  // console.log(campgrounds.avgRating);
   res.render("campgrounds/index", { campgrounds });
 };
 
@@ -17,7 +18,9 @@ module.exports.indexSorted = async (req, res) => {
 };
 module.exports.impressionSorted = async (req, res) => {
   const sortedArr = await Campground.find({}).populate("popupText");
-  const campgrounds = sortedArr.sort((a, b) => b.impressionCount - a.impressionCount);
+  const campgrounds = sortedArr.sort(
+    (a, b) => b.impressionCount - a.impressionCount
+  );
   res.render("campgrounds/index", { campgrounds });
 };
 
@@ -54,16 +57,17 @@ module.exports.showCampground = async (req, res) => {
       },
     })
     .populate("author");
-    const protocol = req.protocol;
-    const host = req.hostname;
-    const url = req.originalUrl;
-    const port = process.env.PORT || PORT;
-    const fullUrl = `${protocol}://${host}${url}`;
-  const wa = "whatsapp://send?text=Come and check " + campground.title + " 🤗, a beautiful place😌. " + "Do comment and rate my camps😉.%0a" + fullUrl; 
-<<<<<<< HEAD
-  const te = "Come and check " + campground.title + " 🤗, a beautiful place😌. " + "Do comment and rate my camps😉.%0a" + fullUrl;
-  console.log(fullUrl);
-=======
+  const protocol = req.protocol;
+  const host = req.hostname;
+  const url = req.originalUrl;
+  const port = process.env.PORT || PORT;
+  const fullUrl = `${protocol}://${host}${url}`;
+  const wa =
+    "whatsapp://send?text=Come and check " +
+    campground.title +
+    " 🤗, a beautiful place😌. " +
+    "Do comment and rate my camps😉.%0a" +
+    fullUrl;
   const te =
     "Come and check " +
     campground.title +
@@ -71,7 +75,6 @@ module.exports.showCampground = async (req, res) => {
     "Do comment and rate my camps😉.%0a" +
     fullUrl;
 
->>>>>>> 37a10ee (commit)
   const { id } = req.params;
   const campgrounds = await Campground.findByIdAndUpdate(id, {
     impressionCount: campground.impressionCount + 1,
@@ -79,39 +82,36 @@ module.exports.showCampground = async (req, res) => {
   await campgrounds.save();
   const place = campground.location;
 
-<<<<<<< HEAD
-  const i = response.data.current.condition.icon;
-=======
-const apiKey = "378da041b4f297aa7ab609ee83355815";
-const cityName = place; // Replace with the name of the city you want to get weather data for
+  const apiKey = "378da041b4f297aa7ab609ee83355815";
+  const cityName = place; // Replace with the name of the city you want to get weather data for
 
-// Declare global variables to store temperature and weather icon
-let temperatureCelsius;
-let weatherIconUrl;
-let x = 0;
-const response = await axios
-  .get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
-  )
-  .catch(function (error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      x = 1;
-    }
-  });
-if (x == 0) {
-  const temperatureKelvin = response.data.main.temp;
+  // Declare global variables to store temperature and weather icon
+  let temperatureCelsius;
+  let weatherIconUrl;
+  let x = 0;
+  const response = await axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+    )
+    .catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        x = 1;
+      }
+    });
+  if (x == 0) {
+    const temperatureKelvin = response.data.main.temp;
 
-  // Convert Kelvin to Celsius (optional)
-  temperatureCelsius = (temperatureKelvin - 273.15).toFixed(2);
+    // Convert Kelvin to Celsius (optional)
+    temperatureCelsius = (temperatureKelvin - 273.15).toFixed(2);
 
-  // Get the weather icon code from the API response
-  const weatherIconCode = response.data.weather[0].icon;
+    // Get the weather icon code from the API response
+    const weatherIconCode = response.data.weather[0].icon;
 
-  // Construct the URL for the weather icon image
-  weatherIconUrl = `https://openweathermap.org/img/w/${weatherIconCode}.png`;
-}
+    // Construct the URL for the weather icon image
+    weatherIconUrl = `https://openweathermap.org/img/w/${weatherIconCode}.png`;
+  }
 
   // const response = await axios(
   //   `https://api.weatherapi.com/v1/current.json?q=${place}&aqi=no&key=1b8676ab83734bf88d592311210410`
@@ -120,24 +120,19 @@ if (x == 0) {
   // const temp_c = response.data.current.temp_c;
   // const temp_f = response.data.current.temp_f;
   // const i = response.data.current.condition.icon;
->>>>>>> 37a10ee (commit)
   if (!campground) {
     req.flash("error", "Cannot find that campground!");
     return res.redirect("/campgrounds");
   }
-<<<<<<< HEAD
-  res.render("campgrounds/show", { campground,wa, te, fullUrl });
-=======
- res.render("campgrounds/show", {
-   campground,
-   wa,
-   weatherIconUrl,
-   temperatureCelsius,
-   te,
-   x,
-   fullUrl,
- });
->>>>>>> 37a10ee (commit)
+  res.render("campgrounds/show", {
+    campground,
+    wa,
+    weatherIconUrl,
+    temperatureCelsius,
+    te,
+    x,
+    fullUrl,
+  });
 };
 module.exports.renderEditForm = async (req, res) => {
   const { id } = req.params;
@@ -180,15 +175,20 @@ module.exports.deleteCampground = async (req, res) => {
 module.exports.search = async (req, res) => {
   const { search } = req.body;
   const campgrounds = await Campground.find({
-     "$or": [
-    {title: {
-      $regex: search,
-      $options: "i",
-    }},
-    {location: {
-      $regex: search,
-      $options: "i",}
-    }]
+    $or: [
+      {
+        title: {
+          $regex: search,
+          $options: "i",
+        },
+      },
+      {
+        location: {
+          $regex: search,
+          $options: "i",
+        },
+      },
+    ],
   });
   res.render("campgrounds/index", { campgrounds });
 };
--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,21 +1,28 @@
+const { randomInt } = require("crypto");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
+const review = require("../models/review");
 
 module.exports.createReview = async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
+  // const campground = await Campground.findById(req.params.id);
+  const campground = await Campground.findById(req.params.id).populate(
+    "reviews"
+  );
   const review = new Review(req.body.review);
+  const ratings = campground.reviews.map((review) => review.rating);
   campground.rating.push(review.rating);
     let average = 0; 
-  for (let i = 0; i < campground.rating.length; i++) {
-    average+=campground.rating[i];
-                     }
-                    if(campground.rating.length){
-                    
-                      average = average / campground.rating.length;
-                    }
-  campground.avgRating = average;
+  
   review.author = req.user._id;
   campground.reviews.push(review);
+  let x = ratings.length;
+
+  for (let i = 0; i < x ; i++)
+    average += ratings[i];
+  average += review.rating;
+  average /= (x + 1);
+  console.log(average);
+  campground.avgRating = average;
   await review.save();
   await campground.save();
   req.flash("success", "Created new review!");
@@ -24,10 +31,32 @@ module.exports.createReview = async (req, res) => {
 
 module.exports.deleteReview = async (req, res) => {
   const { id, reviewId } = req.params;
+  // const campgroundd = await Campground.findById(req.params.id);
+   const campground = await Campground.findById(req.params.id).populate(
+     "reviews"
+   );
+  const review = new Review(req.body.review);
+  
+  const ratings = campground.reviews.map((review) => review.rating);
+  
   const rating = (await Review.findById(reviewId)).rating;
+  let average = 0;
+  let x = ratings.length;
+
+  for (let i = 0; i < x; i++) average += ratings[i];
+  average -= rating;
+  console.log(average);
+  average /= (x - 1);
+  console.log(average);
+  campground.avgRating = average;
+  
+ 
+console.log(campground);
   await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
   await Campground.findByIdAndUpdate(id, { $pull: { rating: rating } });
-  await Review.findByIdAndDelete(reviewId);
   req.flash("success", "Successfully deleted review");
+  console.log(campground);
+   await campground.save();
+   await review.save();
   res.redirect(`/campgrounds/${id}`);
 };
